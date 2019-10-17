7dde250cb672764ba8a2478eaae0cc7ecbd74dfa
Omar Rios <omarios90@gmail.com>
2019-10-18 00:28:40 +0200

    mejora sobre la primera versión

--- ejercicio 1/main.py
+++ ejercicio 1/main.py
@@ -1,10 +1,8 @@
 def get_divisors(num):
     result = set()
     result.add(1)
-    for i in range(2, int(num/2) + 1):
+    for i in range(2, int(num ** 0.5) + 1):
         div = num / i
-        if div < i:
-            break
         if div % 1 == 0:
             result.add(i)
             result.add(int(div))
